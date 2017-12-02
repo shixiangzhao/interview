38919721f3b09e531d597797015626accfe72d87
Zhao <zsxfordream@163.com>
2017-12-02 09:05:43 +0800

    test float

---
+++ src/main/java/com/shixzh/java/interview/basic/TestFloat.java
@@ -0,0 +1,12 @@
+package com.shixzh.java.interview.basic;
+
+public class TestFloat {
+
+	public static void main(String[] args) {
+		double f = 3 * 0.1;
+		System.out.println(f);
+		System.out.println(3 * 0.1 == 0.3);
+
+		Double d;
+	}
+}
