3ec636c309e11386a6ed2a3ab2819560bf17fc2d
Zhao <zsxfordream@163.com>
2018-01-10 22:37:54 +0800

    update Instrument hash set and update generic effective java

--- src/main/java/com/shixzh/java/interview/generic/GenericInEffectiveJava.java
+++ src/main/java/com/shixzh/java/interview/generic/GenericInEffectiveJava.java
@@ -3,31 +3,61 @@ package com.shixzh.java.interview.generic;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Set;
 
+/**
+ * 第23条：请不要在新代码中使用原生态类型（相对于泛型<>）
+ * 
+ * @author Shixiang
+ *
+ */
 public class GenericInEffectiveJava {
 
-    private static final Collection stamps = new ArrayList();
-    private static final Collection<Stamp> stamps1 = new ArrayList();
+	private static final Collection stamps = new ArrayList();
+	private static final Collection<Stamp> stamps1 = new ArrayList();
 
-    public static void main(String[] args) {
-        // Erroneous insertion of coin into stamp collection
-        stamps.add(new Coin());
-        // Not applicable for the arguments (Coin)
-        //stamps1.add(new Coin());
+	// Use of raw type for unknown element type - don't do this
+	static int numElementsInCommon(Set s1, Set s2) {
+		int result = 0;
+		for (Object o1 : s1)
+			if (s2.contains(o1))
+				result++;
+		return result;
+	}
 
-        // Now a raw iterator type - don't do this!
-        for (Iterator i = stamps.iterator(); i.hasNext();) {
-            Stamp s = (Stamp) i.next();// Throws ClassCaseException
-        }
-        // for-each loop over a parameterized collection - typesafe
-        for (Stamp s : stamps1) { //no cast
+	// Unbounded wildcard type - typesafe and flexible通配符类型（bounded wildcard type）
+	static int numElementsInCommon1(Set<?> s1, Set<?> s2) {
+		int result = 0;
+		for (Object o1 : s1) {
+			if (s2.contains(o1))
+				result++;
+			// Legitimate use of raw type - instanceof operator
+			if (o1 instanceof Set) // row type
+				s1 = (Set<?>) o1; // wildcard type
+		}
+		return result;
+	}
 
-        }
-        // for loop with parameterized iterator declaration - typesafe
-        for (Iterator<Stamp> i = stamps.iterator(); i.hasNext();) {
-            Stamp s = i.next();//no cast necessary
-        }
-    }
+	public static void main(String[] args) {
+		// Erroneous insertion of coin into stamp collection
+		stamps.add(new Coin());
+		// Not applicable for the arguments (Coin)
+		// stamps1.add(new Coin());
+
+		// Now a raw iterator type - don't do this!
+		for (Iterator i = stamps.iterator(); i.hasNext();) {
+			Stamp s = (Stamp) i.next();// Throws ClassCaseException
+		}
+		// for-each loop over a parameterized collection - typesafe
+		for (Stamp s : stamps1) { // no cast
+
+		}
+		// for loop with parameterized iterator declaration - typesafe
+		for (Iterator<Stamp> i = stamps.iterator(); i.hasNext();) {
+			Stamp s = i.next();// no cast necessary
+		}
+
+	}
 
 }
 
--- src/main/java/com/shixzh/java/interview/interfaces/InstrumentedHashSet.java
+++ src/main/java/com/shixzh/java/interview/interfaces/InstrumentedHashSet.java
@@ -2,48 +2,165 @@ package com.shixzh.java.interview.interfaces;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * 第16条：复合(composition)/转发(forwarding)优先于继承
+ * 
  * @author shixiang.zhao
  *
  * @param <E>
  */
 public class InstrumentedHashSet<E> extends HashSet<E> {
 
-    private int addCount = 0;
+	private static final long serialVersionUID = 1L;
+	private int addCount = 0;
 
-    public InstrumentedHashSet() {
+	public InstrumentedHashSet() {
 
-    }
+	}
 
-    public InstrumentedHashSet(int initCap, float loadFactor) {
-        super(initCap, loadFactor);
-    }
+	public InstrumentedHashSet(int initCap, float loadFactor) {
+		super(initCap, loadFactor);
+	}
 
-    @Override
-    public boolean add(E e) {
-        addCount++;
-        return super.add(e);
-    }
+	@Override
+	public boolean add(E e) {
+		addCount++;
+		return super.add(e);
+	}
 
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        addCount += c.size();
-        return super.addAll(c);
-    }
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		addCount += c.size();
+		return super.addAll(c);
+	}
 
-    public int getAddCount() {
-        return addCount;
-    }
+	public int getAddCount() {
+		return addCount;
+	}
 
-    public static void main(String[] args) {
-        InstrumentedHashSet<String> s = new InstrumentedHashSet<>();
-        s.addAll(Arrays.asList("Snap", "Crackle", "Pop"));
-        System.out.println("Expert 3, but: " + s.getAddCount());
-        System.out.println(s);
+	public static void main(String[] args) {
+		InstrumentedHashSet<String> s = new InstrumentedHashSet<>();
+		s.addAll(Arrays.asList("Snap", "Crackle", "Pop"));
+		System.out.println("Expert 3, but: " + s.getAddCount());
+		System.out.println(s);
 
-    }
+		// 有了转发类，addAll()方法就可以不回调自己的add()方法啦。
+		InstrumentedHashSet1<String> s1 = new InstrumentedHashSet1<>(new HashSet<String>());
+		s1.addAll(Arrays.asList("Snap", "Crackle", "Pop"));
+		System.out.println("Expert 3, and: " + s1.getAddCount());
+
+		// Set<Date> s3 = new InstrumentedHashSet1<>(new TreeSet<Date>(cmp));
+		// Set<E> s4 = new InstrumentedHashSet1<E>(new HashSet<Date>(capacity));
+
+	}
+
+}
+
+class InstrumentedHashSet1<E> extends ForwardingSet<E> {
+
+	private int addCount = 0;
+
+	public InstrumentedHashSet1(Set<E> s) {
+		super(s);
+	}
+
+	@Override
+	public boolean add(E e) {
+		addCount++;
+		return super.add(e);
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		addCount += c.size();
+		return super.addAll(c);
+	}
+
+	public int getAddCount() {
+		return addCount;
+	}
+
+}
+
+// Reusable forwarding class
+class ForwardingSet<E> implements Set<E> {
+
+	// 复合composition/转发forwarding
+	private final Set<E> s;
+
+	public ForwardingSet(Set<E> s) {
+		this.s = s;
+	}
+
+	@Override
+	public int size() {
+		return s.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return s.isEmpty();
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		return s.contains(o);
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return s.iterator();
+	}
+
+	@Override
+	public Object[] toArray() {
+		return s.toArray();
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		return s.toArray(a);
+	}
+
+	@Override
+	public boolean add(E e) {
+		return s.add(e);
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		return s.remove(o);
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		return s.containsAll(c);
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		return s.addAll(c);
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		return s.retainAll(c);
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		return s.removeAll(c);
+	}
+
+	@Override
+	public void clear() {
+		s.clear();
+	}
 
 }
