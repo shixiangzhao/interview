412bccdc1f56f10dd1c71feb8ce98c992ac5b748
Shixiang Zhao <zsxfordream@163.com>
2018-03-21 19:34:14 +0800

    update CustomLinkedHashMap.java

--- src/main/java/com/shixzh/java/interview/collections/CustomLinkedHashMap.java
+++ src/main/java/com/shixzh/java/interview/collections/CustomLinkedHashMap.java
@@ -7,13 +7,18 @@ import java.util.Map;
 public class CustomLinkedHashMap<K, V> {
 
 	public static void main(String[] args) {
-		Map<String, Integer> lhm = new LinkedHashMap<>();
+		Map<String, Integer> lhm = new LinkedHashMap<>(16, 0.75f, true);
 		lhm.put("ZhangSan", 3);
 		lhm.put("LiSi", 2);
 		lhm.put("WangWu", 6);
 		for (java.util.Map.Entry<String, Integer> e : lhm.entrySet()) {
 			System.out.println("key: " + e.getKey() + " value: " + e.getValue());
 		}
+		int count = lhm.get("LiSi");
+        System.out.println("lhm.get(): " + count +"， 顺序发生改变：");
+        for (java.util.Map.Entry<String, Integer> e : lhm.entrySet()) {
+            System.out.println("key: " + e.getKey() + " value: " + e.getValue());
+        }
 		System.out.println("==========HashMap=======");
 		Map<String, Integer> hm = new HashMap<>();
 		hm.put("ZhangSan", 3);
